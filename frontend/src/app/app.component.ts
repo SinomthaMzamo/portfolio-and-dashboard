2ada5fbe346393e207b59f284b904c1e0048ffb2
Sinomtha Mzamo <sinomtha.mzamo@safetyio.com>
2025-08-01 16:00:55 +0200

    Added landing section and utility classes like container and section

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./shared/navbar/navbar.component";
+import { AboutComponent } from "./ui/about/about.component";
+import { LandingComponent } from "./ui/landing/landing.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, AboutComponent, LandingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
